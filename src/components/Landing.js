4be2674209f1cbe691330e8a90b4e1632b645404
Manindra de Mel <u7156805@anu.edu.au>
2023-06-17 18:31:43 +1000

    landing page working

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Landing.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faLinkedin, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+
 
 const Landing = () => {
     return (
@@ -13,9 +16,15 @@ const Landing = () => {
                 and continuously learning and growing in this dynamic field. In my free time, I love exploring new tech trends and expanding my skill set..
                 </p>
                 <div className="social-links">
-                    <a href="[your LinkedIn URL]" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                    <a href="[your GitHub URL]" target="_blank" rel="noopener noreferrer">GitHub</a>
-                    <a href="[your Instagram URL]" target="_blank" rel="noopener noreferrer">Instagram</a>
+                    <a href="[your LinkedIn URL]" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} size="2x" color="white"/>
+                    </a>
+                    <a href="[your GitHub URL]" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faGithubSquare} size="2x" color="white"/>
+                    </a>
+                    <a href="[your Instagram URL]" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagramSquare} size="2x" color="white"/>
+                    </a>
                 </div>
                 <div className="cta-container">
                     <a href="#contact" className="cta-btn">Get in Touch</a>
