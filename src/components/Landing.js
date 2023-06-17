93aaaa79900c8bcd60d78675bc38fc737ab192d9
Manindra de Mel <u7156805@anu.edu.au>
2023-06-17 17:41:17 +1000

    landing init

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,13 +3,16 @@ import './Landing.css';
 
 const Landing = () => {
     return (
-        <section id="introduction" className="intro">
-            <div className="intro-content">
-                <h1>Hello, I'm <span>Manindra de Mel</span></h1>
-                <p>I'm a Full Stack Developer</p>
+        <section id="introduction" className="landing">
+            <div className="landing-content">
+                <h1>Hello, I'm [Your Name]</h1>
+                <p className="profession-text">I'm a [Your Profession]</p>
+                <div className="cta-container">
+                    <a href="#contact" className="cta-btn">Get in Touch</a>
+                </div>
             </div>
-            <div className="intro-image">
-                <img src="/images/me.jpg" alt="John Doe" />
+            <div className="landing-image">
+                <img src="path-to-your-image" alt="A portrait of you" />
             </div>
         </section>
     );
