249793aa9f40844b489c6d4fdcdb540c838bea4a
Manindra de Mel <u7156805@anu.edu.au>
2023-06-17 18:14:58 +1000

    landing change

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,14 +5,24 @@ const Landing = () => {
     return (
         <section id="introduction" className="landing">
             <div className="landing-content">
-                <h1>Hello, I'm [Your Name]</h1>
-                <p className="profession-text">I'm a [Your Profession]</p>
+                <h1>Manindra de Mel</h1>
+                <p className="profession-text">I'm a Full stack developer</p>
+                <p className="short-description">
+                Based in the heart of Canberra, I bring over 5 years of extensive experience in the IT industry, where I have honed my skills in various domains. 
+                What truly drives me is the thrill of solving complex computer science problems, designing innovative and effective solutions, 
+                and continuously learning and growing in this dynamic field. In my free time, I love exploring new tech trends and expanding my skill set..
+                </p>
+                <div className="social-links">
+                    <a href="[your LinkedIn URL]" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    <a href="[your GitHub URL]" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href="[your Instagram URL]" target="_blank" rel="noopener noreferrer">Instagram</a>
+                </div>
                 <div className="cta-container">
                     <a href="#contact" className="cta-btn">Get in Touch</a>
                 </div>
             </div>
             <div className="landing-image">
-                <img src="path-to-your-image" alt="A portrait of you" />
+                <img src="/assets/profile.jpg" alt="A portrait of me" />
             </div>
         </section>
     );
