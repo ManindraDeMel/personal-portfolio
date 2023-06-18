84ee298a88605ca0bce30b31716f9f0a93007943
Manindra de Mel <u7156805@anu.edu.au>
2023-06-19 00:10:38 +1000

    Finished navbar mostly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <nav className="navbar">
-            <ul>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            <ul className={`nav-links ${isOpen ? 'showMenu' : ''}`}>
                 <li><ScrollLink to="introduction" smooth={true} duration={1000} offset={-70}>Home</ScrollLink></li>
                 <li><ScrollLink to="journey" smooth={true} duration={1000} offset={70}>Journey</ScrollLink></li>
                 <li><ScrollLink to="resume" smooth={true} duration={1000} offset={-250}>Resume</ScrollLink></li>
