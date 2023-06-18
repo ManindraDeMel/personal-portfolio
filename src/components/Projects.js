dc2c22893ad8aa85d50b15efc10a6bf105b7ad0d
Manindra de Mel <u7156805@anu.edu.au>
2023-06-18 18:43:04 +1000

    Finished projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Masonry from 'masonry-layout';
 import './Projects.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 function Projects() {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/user/repos', {
+            headers: {
+                'Authorization': `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => setRepos(data));
+    }, []);
+
+    useEffect(() => {
+        AOS.refresh();
+    }, [repos]);
+    
+
     useEffect(() => {
         new Masonry('.project-grid', {
             itemSelector: '.project-card',
@@ -11,25 +30,20 @@ function Projects() {
             gutter: 20,
             originLeft: true,
         });
-    }, []);
+    }, [repos]);
 
     return (
         <div className="project-section" id="projects">
             <h2>My Projects</h2>
             <div className="project-grid">
-                <div className="project-card" style={{ height: `${Math.random() * 100 + 150}px` }}>
-                    <h3>Project 1</h3>
-                    <p>January 2022</p>
-                    <p>A short description of Project 1.</p>
-                    <a href="https://github.com/username/project1" target="_blank" rel="noopener noreferrer">View on GitHub</a>
-                </div>
-                <div className="project-card" style={{ height: `${Math.random() * 100 + 150}px` }}>
-                    <h2>Project 2</h2>
-                    <p>February 2022</p>
-                    <p>A short description of Project 2.</p>
-                    <a href="https://github.com/username/project2" target="_blank" rel="noopener noreferrer">View on GitHub</a>
-                </div>
-                {/* Repeat for each project */}
+                {repos.map(repo => (
+                    <div className="project-card" key={repo.id} data-aos="fade-up">
+                        <h3>{repo.name}</h3>
+                        <p>Last Updated: {new Date(repo.updated_at).toLocaleDateString()}</p>
+                        <p>{repo.description || ''}</p>
+                        {!repo.private && <a href={repo.html_url} target="_blank" rel="noopener noreferrer">View on GitHub</a>}
+                    </div>
+                ))}
             </div>
         </div>
     );
