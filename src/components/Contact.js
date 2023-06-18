294d0309e2d461c335ad80e45e614b169532f2f8
Manindra de Mel <u7156805@anu.edu.au>
2023-06-19 03:09:37 +1000

    Finished contact (for now)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,9 @@ import './Contact.css';
 
 function Contact() {
     return (
-        <div className="contact-section" data-aos="fade-up">
-            <h1>Let's work together</h1>
-            <div className="contact-info">
+        <div className="contact-section">
+            <h1 data-aos="fade-up">Let's work together</h1>
+            <div className="contact-info" data-aos="fade-up">
                 <div className="info-column">
                     <h2>Inquire</h2>
                     <p>manindrademel@yahoo.com.au</p>
@@ -18,7 +18,7 @@ function Contact() {
                     <p>Gunghalin Area, Canberra ACT, 2914 AUSTRALIA</p>
                 </div>
             </div>
-            <div className="social-links">
+            <div className="social-links" data-aos="fade-up">
                 <a href="https://www.linkedin.com/in/manindra-de-mel-413a79160/" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLinkedin} size="2x" color="white"/>
                 </a>
