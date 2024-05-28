851525c3db9c2af4cfe7a2648d2bdadc369da0c9
Manindra de Mel <u7156805@anu.edu.au>
2024-05-29 03:42:30 +1000

    add unique filter to data

--- src/service/fetchTimeline.js
+++ src/service/fetchTimeline.js
@@ -1,11 +1,29 @@
-// fetchTimelineData.js
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 import { db } from '../firebase';
 
 async function fetchTimelineData() {
-    const querySnapshot = await getDocs(collection(db, 'timeline'));
-    const timelineData = querySnapshot.docs.map(doc => doc.data());
-    return timelineData;
+    try {
+        const q = query(collection(db, 'timeline'), orderBy('year', 'desc'));
+        const querySnapshot = await getDocs(q);
+
+        const seen = new Set();
+        const timelineData = [];
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const identifier = `${data.year}-${data.subtitle}`;
+
+            if (!seen.has(identifier)) {
+                seen.add(identifier);
+                timelineData.push(data);
+            }
+        });
+
+        return timelineData;
+    } catch (error) {
+        console.error('Error fetching data from Firestore:', error);
+        throw error; // Re-throw the error after logging it
+    }
 }
 
 export default fetchTimelineData;
